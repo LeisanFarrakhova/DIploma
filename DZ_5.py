638e9fa2873e86d36b8ca19fc85ec3494310db1d
Leisan Farrakhova <flm8952@gmail.com>
2026-08-01 19:50:49 +0300

    1 этап_DZ 5

---
+++ DZ_5.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox as mb
+import requests
+import json
+
+
+window = Tk()
+window.title("Курс обмена валюты ")
+window.geometry("460x520")
+
+window.mainloop()
